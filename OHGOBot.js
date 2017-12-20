73fb3ffc7a692db0e2e62995de77e0d0771599eb
justins83 <justins83.waze@gmail.com>
2017-12-20 09:12:45 -0500

    Update OHGOBot.js

--- OHGOBot.js
+++ OHGOBot.js
@@ -8,13 +8,13 @@ var parseString = require('xml2js').parseString;
 var AWS = require('aws-sdk');
 const readline = require('readline');
 var inside = require('point-in-polygon');
+
 var NW = [ [ 41.69621150, -84.80620470 ], [ 41.74707970, -82.71565040 ], [40.57368660, -82.68183770 ], [ 40.54522630, -84.80279780 ] ];
 var NE = [[41.62058260,-82.71194720],[41.98212010,-80.51942980],[40.56682920,-80.51892620],[40.57368660,-82.68183770]];
 var SW = [[40.54522630,-84.80279780],[40.56826300,-83.41627120],[38.42400810,-83.37387080],[39.09809450,-84.88311770]];
 var SE = [[39.70218290,-83.39936390],[39.71772530,-80.75030830],[38.36184910,-82.40226750],[38.60882250,-83.37799080]];
 var C = [[40.56826300,-83.41626050],[40.56768290,-80.58881650],[39.71669450,-80.59157260],[39.70218290,-83.39935320]];
  
-//https://discordapp.com/api/webhooks/392727461953011713/9iD33ib0JjD0RU-PWCoL9KN_VNJ3jrIiDfvAybQTvOcyU8qhU_rPrzgR2TgvczHOXp3z
 const webhook = "https://discordapp.com/api/webhooks/392727461953011713/9iD33ib0JjD0RU-PWCoL9KN_VNJ3jrIiDfvAybQTvOcyU8qhU_rPrzgR2TgvczHOXp3z"; //test server webhook - "https://discordapp.com/api/webhooks/391299150211317761/tntWFj2dMjJi7JGJrn_bjMm_rg6REL8DugFpxQ5MqrByMkMjLy3M-_EJ3CVVK9_lM_Rt";
 const url = "http://www.buckeyetraffic.org/services/roadactivity.aspx";
 var postedIDs = [];
@@ -113,12 +113,12 @@ function xmlToJson(url, callback) {
 function WriteToFile()
 {
 	//fs.unlinkSync('posted.txt');
-	var file = fs.createWriteStream('parsed.txt');
+	/*var file = fs.createWriteStream('parsed.txt');
 	file.on('error', function(err) { console.log(err);});
 	postedIDs.forEach(function(v) {if(v != ""){ file.write(v + '\n'); }});
-	file.end();
+	file.end();*/
 	
-	/*let params = {Bucket: myBucket, Key: myKey, Body: postedIDs.join('\n')};
+	let params = {Bucket: myBucket, Key: myKey, Body: postedIDs.join('\n')};
      s3.putObject(params, function(err, data) {
          if (err) {
              console.log(err)
@@ -126,19 +126,19 @@ function WriteToFile()
              console.log("Successfully uploaded data to myBucket/myKey");
          }
 
-      });*/
+      });
 }
 
 function ReadFromFile()
 {
-	if(fs.existsSync('posted.txt'))
+	/*if(fs.existsSync('posted.txt'))
 	{
 		var array = fs.readFileSync('parsed.txt').toString().split("\n");
 		for(i in array) {
 			postedIDs.push(array[i]);
 		}
-	}
-	/*
+	}*/
+	
 	console.log("Reading S3");
 	let params = {Bucket: myBucket, Key: myKey};
 	const rl = readline.createInterface({
@@ -150,18 +150,13 @@ function ReadFromFile()
 		console.log(line);
 	})
 	.on('close', function() {
-	});*/
+		xmlToJson(url, scrapeOHGO);
+		setInterval(function(){
+			console.log("Interval!");
+			xmlToJson(url, scrapeOHGO);}, 600000);
+	});
 }
-var NWx=[41.69621150,41.74707970,40.57368660,40.54522630];
-var NWy=[-84.80620470,-82.71565040,-82.68183770,-84.80279780];
-var NEx=[41.62058260,41.98212010,40.56682920,40.57368660];
-var NEy=[-82.71194720,-80.51942980,-80.51892620,-82.68183770];
-var SWx=[40.54522630,40.56826300,38.42400810,39.09809450];
-var SWy=[-84.80279780,-83.41627120,-83.37387080,-84.88311770];
-var SEx=[39.70218290,39.71772530,38.36184910,38.60882250];
-var SEy=[-83.39936390,-80.75030830,-82.40226750,-83.37799080];
-var Cx=[40.56826300,40.56768290,39.71669450,39.70218290];
-var Cy=[-83.41626050,-80.58881650,-80.59157260,-83.39935320];
+
 
 function checkcheck (x, y, cornersX, cornersY) {
 
@@ -180,12 +175,5 @@ function checkcheck (x, y, cornersX, cornersY) {
 
 	  return oddNodes;
 }
-	
 
 ReadFromFile();
-xmlToJson(url, scrapeOHGO);
-setInterval(function(){
-	console.log("Interval!");
-	xmlToJson(url, scrapeOHGO);}, 600000);
-	
-	
